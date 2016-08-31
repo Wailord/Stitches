ad2b5f128677cc92de349c9bb4f10fc7056066c9
Ryan Fox <rfox@c02rp985fvh5.local>
2016-08-31 12:20:35 -0500

    Initial Commit

---
+++ Stitches/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stitches
+//
+//  Created by Ryan Fox on 8/31/16.
+//  Copyright © 2016 Ryan Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
