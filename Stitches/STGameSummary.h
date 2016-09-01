d949455e8013be4bb756fc86f1271c70599df4bd
Ryan Fox <rfox@c02rp985fvh5.local>
2016-09-01 09:20:56 -0500

    added a game summary model

---
+++ Stitches/STGameSummary.h
@@ -0,0 +1,21 @@
+//
+//  STGameSummary.h
+//  Stitches
+//
+//  Created by Ryan Fox on 9/1/16.
+//  Copyright © 2016 Ryan Fox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STGameSummary : NSObject
+
+@property NSString *awayTeam;
+@property NSNumber *awayScore;
+@property NSString *homeTeam;
+@property NSNumber *homeScore;
+
+@property NSNumber *inning;
+@property BOOL topOfInning;
+
+@end
---
+++ Stitches/STGameSummary.m
@@ -0,0 +1,13 @@
+//
+//  STGameSummary.m
+//  Stitches
+//
+//  Created by Ryan Fox on 9/1/16.
+//  Copyright © 2016 Ryan Fox. All rights reserved.
+//
+
+#import "STGameSummary.h"
+
+@implementation STGameSummary
+
+@end
